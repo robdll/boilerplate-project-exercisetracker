5c7450f9cd695194db93eb83dbeebb35e2917b98
Roberto Di Lillo <dilillo.roberto@gmail.com>
2022-07-08 00:22:56 +0100

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,10 @@
+let mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true],
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema)
